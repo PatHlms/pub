c485cfbee6764d0e8c08abfdb19dd449f4203fa9
Patrick Holmes <patrick@patrickholmes.com>
2026-02-16 21:00:52 +0000

    Removing skel

--- lima/v6/oil_temperature_fault_code_reader.py
+++
@@ -1,7 +0,0 @@
-from .logging.logger import logger
-
-class OilTemperatureFaultCodeReader:
-    def read_fault_code(self):
-        logger.info('Reading oil temperature fault code from engine management system')
-        # Placeholder: Simulate reading oil temperature fault code
-        return {'code': 'P5678', 'description': 'Oil temperature too high'}
--- lima/v6/turbocharger_fault_code_reader.py
+++
@@ -1,7 +0,0 @@
-from .logging.logger import logger
-
-class TurbochargerFaultCodeReader:
-    def read_fault_code(self):
-        logger.info('Reading turbocharger fault code from engine management system')
-        # Placeholder: Simulate reading turbocharger fault code
-        return {'code': 'P1234', 'description': 'Turbocharger underboost'}
